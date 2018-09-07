1e4b79609d7ca1af384b327e233283cfab6d4a45
alwaleed <aalturki1@live.maryville.edu>
2018-09-07 12:37:44 -0500

    1.1 song lyrics

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -7,17 +7,17 @@ print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
-/*:
- Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
- */
 
+
+print("Smoke clear")
+print("Andy Grammer")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("You will never be alone")
+print("I will be right here")
+print("I took my hardest fall")
 /*:
  
  _Copyright © 2017 Apple Inc._
